18373aa20ddf63de9d966b8b269db500dfd0859d
abigailkeller <abigail.g.keller1@gmail.com>
2026-08-21 03:42:44 +0000

    updated model

--- code/summarize_posterior.R
+++ code/summarize_posterior.R
@@ -1,5 +1,6 @@
 library(MCMCvis)
 library(tidyverse)
+library(patchwork)
 
 # read in samples
 samples <- readRDS("data/posterior_samples/posterior_samples.rds")
@@ -8,18 +9,90 @@ samples <- readRDS("data/posterior_samples/posterior_samples.rds")
 summary <- MCMCsummary(samples)
 
 # trace plot
-param <- "z[73, 6]"
+param <- "z[113, 5]"
+param <- "p_minnow"
 
 ggplot() +
-  geom_line(aes(x = 1:nrow(out_sub[[1]]), y = out_sub[[1]][, param]),
+  geom_line(aes(x = 1:nrow(samples[[1]]), y = samples[[1]][, param]),
             color = "blue") +
-  geom_line(aes(x = 1:nrow(out_sub[[2]]), y = out_sub[[2]][, param]),
+  geom_line(aes(x = 1:nrow(samples[[2]]), y = samples[[2]][, param]),
             color = "red") +
-  geom_line(aes(x = 1:nrow(out_sub[[3]]), y = out_sub[[3]][, param]),
+  geom_line(aes(x = 1:nrow(samples[[3]]), y = samples[[3]][, param]),
             color = "purple") +
-  geom_line(aes(x = 1:nrow(out_sub[[4]]), y = out_sub[[4]][, param]),
-            color = "green")
+  geom_line(aes(x = 1:nrow(samples[[4]]), y = samples[[4]][, param]),
+            color = "green") +
+  labs(x = "iteration", y = "value") +
+  ggtitle(param)
 
 
 ## Note: some Rhat > 1.1 (will discuss)
 not_converged <- summary[which(summary$Rhat > 1.1), ]
+
+# summarize prob detection
+summary[c("p_minnow", "p_shrimp", "p_fukui"), ]
+
+# summarize beta
+summary[c("beta0", "beta1", "beta2", "beta3", "beta4"), ]
+
+# read in detection data
+detections <- readRDS("data/model_data/PresenceArrayBinary.rds")[, 2:7, ]
+remove <- as.integer(which(apply(detections, 1, function(s) all(is.na(s)))))
+detections <- detections[-remove, , ]
+
+# example posterior of a z
+ggplot() +
+  geom_histogram(aes(x = c(samples[[1]][, "z[74, 1]"], 
+                           samples[[2]][, "z[74, 1]"],
+                           samples[[3]][, "z[74, 1]"],
+                           samples[[4]][, "z[74, 1]"]))) +
+  labs(x = "z, site 74, year 1", y = "count") +
+  theme_minimal()
+
+# look at specific sites
+nicks_lagoon <- rowSums(detections[74, , ], na.rm = TRUE)
+seabeck_conf <- rowSums(detections[103, , ], na.rm = TRUE)
+seabeck_marina <- rowSums(detections[104, , ], na.rm = TRUE)
+ind_island <- rowSums(detections[58, , ], na.rm = TRUE)
+jimmy <- rowSums(detections[60, , ], na.rm = TRUE)
+
+site_ind <- 74
+summary[c(paste0("z[", site_ind, ", 1]"), paste0("z[", site_ind, ", 2]"),
+          paste0("z[", site_ind, ", 3]"), paste0("z[", site_ind, ", 4]"), 
+          paste0("z[", site_ind, ", 5]"), paste0("z[", site_ind, ", 6]")), ]
+
+# function to plot observations and posterior means
+plot_z <- function(index, name) {
+  
+  det_summary <- rowSums(detections[index, , ], na.rm = TRUE)
+  
+  detections <- ggplot() +
+    geom_point(aes(x = 1:6, y = det_summary)) +
+    labs(x = "time", y = "# detections") +
+    ggtitle(name) +
+    theme_minimal()
+  
+  post_summary <- summary[c(paste0("z[", index, ", 1]"), 
+                            paste0("z[", index, ", 2]"),
+                            paste0("z[", index, ", 3]"), 
+                            paste0("z[", index, ", 4]"), 
+                            paste0("z[", index, ", 5]"), 
+                            paste0("z[", index, ", 6]")), ]
+  
+  post <- ggplot() +
+    geom_point(aes(x = 1:6, y = post_summary[, "mean"])) +
+    geom_errorbar(aes(x = 1:6, ymin = post_summary[, "2.5%"],
+                      ymax = post_summary[, "97.5%"])) +
+    labs(x = "time", y = "z (occupancy state)") +
+    theme_minimal()
+  
+  return(detections + post + plot_layout(ncol = 1))
+    
+}
+
+plot_z(74, "Nick's Laggon")
+plot_z(103, "Seabeck Conference")
+plot_z(104, "Seabeck Marina")
+plot_z(58, "Indian Island")
+plot_z(60, "Jimmycomelately")
+
+
